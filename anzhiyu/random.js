b3357a1d2b9f464ac0547a769c340317695df43d
JC-Xiong <2927917567@qq.com>
2023-09-12 22:03:18 +0800

    Site updated: 2023-09-12 22:03:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/09/10/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/09/12/JS学习中prototype、__proto__与constructor/","2023/09/10/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
