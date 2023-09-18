48e648e11a3bc2ba59f0b356e0acf4dc8231d3bb
JC-Xiong <2927917567@qq.com>
2023-09-18 09:13:38 +0800

    Site updated: 2023-09-18 09:13:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/7fb984f.html","posts/524c5408.html","posts/4a17b156.html","posts/48c3b5d3.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/524c5408.html","posts/4a17b156.html","posts/48c3b5d3.html","posts/7fb984f.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
