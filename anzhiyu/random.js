5ee13fbdf66742a05663d531f1d59df1660df6c9
JC-Xiong <2927917567@qq.com>
2023-09-25 16:57:00 +0800

    Site updated: 2023-09-25 16:56:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/7fb984f.html","posts/524c5408.html","posts/c910ccc9.html","posts/48c3b5d3.html","posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/7fb984f.html","posts/524c5408.html","posts/c910ccc9.html","posts/4a17b156.html","posts/48c3b5d3.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
